17a37fa341f4aa2d73e1beded59f7164ffca86f3
IanChen6 <626614767@qq.com>
2018-01-16 19:27:45 +0800

    自动申报部署

--- celery_demo/views.py
+++ celery_demo/views.py
@@ -128,8 +128,59 @@ def tasks(request):
                     return HttpResponse("job is runing background~")
                 else:
                     return HttpResponse("wrong message")
+
+
+            if post_data['Type'][0] == "TAXAPPLYNORMAL":
+                if post_data['BatchID'] and post_data['BatchYear'] and post_data['BatchMonth'] and post_data[
+                    'CompanyID'] and post_data['CustomerID'] and post_data['User'] and post_data['Pwd'] and \
+                        post_data[
+                            'jobname'] and post_data['jobparams'] and post_data['fw1'] and post_data['fw2'] and post_data['hw1'] and post_data['hw2'] and post_data['fwms'] and post_data['fwyj'] and post_data['hwyj'] and post_data['hwms'] and post_data['djzs'] and post_data['dfzs'] and post_data['dczs'] and post_data['djms'] and post_data['dfms'] and post_data['dcms']:
+                    user = post_data['User'][0]
+                    pwd = post_data['Pwd'][0]
+                    batchid = post_data['BatchID'][0]
+                    batchyear = int(post_data['BatchYear'][0])
+                    batchmonth = int(post_data['BatchMonth'][0])
+                    companyid = int(post_data['CompanyID'][0])
+                    customerid = int(post_data['CustomerID'][0])
+                    jobname = post_data['jobname'][0]
+                    jobparams = post_data['jobparams'][0]
+                    fw1=post_data['fw1'][0]
+                    fw2=post_data['fw2'][0]
+                    hw1=post_data['hw1'][0]
+                    hw2=post_data['hw2'][0]
+                    fwms=post_data['fwms'][0]
+                    fwyj=post_data['fwyj'][0]
+                    hwyj=post_data['hwyj'][0]
+                    hwms=post_data['hwms'][0]
+                    djzs=post_data['djzs'][0]
+                    dfzs=post_data['dfzs'][0]
+                    dczs=post_data['dczs'][0]
+                    djms=post_data['djms'][0]
+                    dfms=post_data['dfms'][0]
+                    dcms=post_data['dcms'][0]
+
+                    # 获取数据库
+                    host, port, db = get_db(companyid)
+                    # 添加任务
+                    logger.info("添加任务到数据库")
+                    logger.info(db)
+                    add_task(host, port, db, batchid, batchyear, batchmonth, companyid, customerid, "TAXAPPLYNORMAL", jobname,
+                             jobparams)
+                    logger.info("任务添加成功,开始爬取")
+                    pdict = {"1": user, "2": pwd, "3": batchid, "4": batchyear, "5": batchmonth, "6": companyid,
+                             "7": customerid, "8": host, "9": port, "10": db,"11": fw1, "12": fw2, "13": hw1, "14": hw2,"15": fwms, "16": fwyj, "17":hwyj, "18": hwms,"19":djzs, "20":dfzs, "21":dczs, "22":djms,"23":dfms, "24":dcms}
+                    pjson = json.dumps(pdict)
+                    redis_cli.lpush("zdsblist", pjson)
+                    # ss=redis_cli.lpop("list")
+                    # print(redis_cli.lpop("list"))
+                    # result=run_test_suit.delay(user=account, pwd=pwd, batchid=batchid, batchyear=batchyear, batchmonth=batchmonth,companyid=companyid, customerid=customerid,host=host,port=port,db=db)
+                    return HttpResponse("job is runing background~")
+                else:
+                    return HttpResponse("wrong message")
+
         except:
             print("wrong message")
+
 
 
     return HttpResponse("input your message")
