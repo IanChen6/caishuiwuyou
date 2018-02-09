9a95bb2a743fd95744038e0967cc2de2e4ecaedd
IanChen6 <626614767@qq.com>
2018-02-09 17:50:27 +0800

    backup

--- celery_demo/views.py
+++ celery_demo/views.py
@@ -193,6 +193,41 @@ def tasks(request):
                 else:
                     return HttpResponse("wrong message")
 
+            if post_data['Type'][0] == "TAXINVOICE":
+                logger.info("抓代开发票")
+                if post_data['BatchID'] and post_data['BatchYear'] and post_data['BatchMonth'] and post_data[
+                    'CompanyID'] and post_data['CustomerID'] and post_data['TaxId'] and post_data['TaxPwd'] and \
+                        post_data['jobname'] and post_data['jobparams']:
+                    logger.info("任务信息接收成功")
+                    user = post_data['TaxId'][0]
+                    pwd = post_data['TaxPwd'][0]
+                    batchid = post_data['BatchID'][0]
+                    batchyear = int(post_data['BatchYear'][0])
+                    batchmonth = int(post_data['BatchMonth'][0])
+                    companyid = int(post_data['CompanyID'][0])
+                    customerid = int(post_data['CustomerID'][0])
+                    jobname = post_data['jobname'][0]
+                    jobparams = post_data['jobparams'][0]
+
+                    # 获取数据库
+                    host, port, db = get_db(companyid)
+                    # 添加任务
+                    logger.info("添加任务到数据库")
+                    logger.info(db)
+                    add_task(host, port, db, batchid, batchyear, batchmonth, companyid, customerid, "TAXINVOICE",
+                             jobname,
+                             jobparams)
+                    logger.info("任务添加成功,开始爬取")
+                    pdict = {"1": user, "2": pwd, "3": batchid,  "4": companyid, "5": customerid, "6": host, "7": port, "8": db}
+                    pjson = json.dumps(pdict)
+                    redis_cli.lpush("daikai", pjson)
+                    # ss=redis_cli.lpop("list")
+                    # print(redis_cli.lpop("list"))
+                    # result=run_test_suit.delay(user=account, pwd=pwd, batchid=batchid, batchyear=batchyear, batchmonth=batchmonth,companyid=companyid, customerid=customerid,host=host,port=port,db=db)
+                    return HttpResponse("job is runing background~")
+                else:
+                    return HttpResponse("wrong message")
+
         except Exception as e:
             print("wrong message")
             logger.warn(e)
@@ -273,11 +308,13 @@ def cancel_task(request):
                     cancelling_task(host, port, db, batchid, batchyear, batchmonth, companyid, customerid,
                                     "CUSTOMERINFO", jobname, jobparams)
                     logger.info("取消任务已添加到数据库")
-                    sz_credit_dict = {"1": user, "2": pwd, "3": batchid, "4": companyid,
-                                      "5": customerid, "6": host, "7": port, "8": db}
-                    pjson = json.dumps(sz_credit_dict)
-                    redis_cli.lrem("sz_credit_list", 1, pjson)
-
+                    # sz_credit_dict = {"1": user, "2": pwd, "3": batchid, "4": companyid,
+                    #                   "5": customerid, "6": host, "7": port, "8": db}
+                    # pjson = json.dumps(sz_credit_dict)
+                    a = redis_cli.lrange('sz_credit_list', 0, -1)
+                    for i in a:
+                        if batchid in i:
+                            redis_cli.lrem('sz_credit_list', 1, i)
                     # ss=redis_cli.lpop("list")
                     # print(redis_cli.lpop("list"))
                     # result=run_test_suit.delay(user=account, pwd=pwd, batchid=batchid, batchyear=batchyear, batchmonth=batchmonth,companyid=companyid, customerid=customerid,host=host,port=port,db=db)
@@ -309,10 +346,14 @@ def cancel_task(request):
                     cancelling_task(host, port, db, batchid, batchyear, batchmonth, companyid, customerid,
                                     "CUSTOMERINVOICE", jobname, jobparams)
                     logger.info("取消任务已添加到数据库")
-                    fphz_dict = {"1": user, "2": pwd, "3": batchid, "4": companyid,
-                                 "5": customerid, "6": host, "7": port, "8": db}
-                    pjson = json.dumps(fphz_dict)
-                    redis_cli.lrem("fphz_list", 1, pjson)
+                    # fphz_dict = {"1": user, "2": pwd, "3": batchid, "4": companyid,
+                    #              "5": customerid, "6": host, "7": port, "8": db}
+                    # pjson = json.dumps(fphz_dict)
+                    # redis_cli.lrem("fphz_list", 1, pjson)
+                    a = redis_cli.lrange('fphz_list', 0, -1)
+                    for i in a:
+                        if batchid in i:
+                            redis_cli.lrem('fphz_list', 1, i)
                     return HttpResponse("job is cancelled successfully~")
                 else:
                     return HttpResponse("wrong message")
@@ -358,16 +399,57 @@ def cancel_task(request):
                     cancelling_task(host, port, db, batchid, batchyear, batchmonth, companyid, customerid,
                                     "TAXAPPLYNORMAL", jobname, jobparams)
                     logger.info("取消任务已添加到数据库")
-                    pdict = {"1": user, "2": pwd, "3": batchid, "4": batchyear, "5": batchmonth, "6": companyid,
-                             "7": customerid, "8": host, "9": port, "10": db, "11": fw1, "12": fw2, "13": hw1,
-                             "14": hw2, "15": fwms, "16": fwyj, "17": hwyj, "18": hwms, "19": djzs, "20": dfzs,
-                             "21": dczs, "22": djms, "23": dfms, "24": dcms}
-                    pjson = json.dumps(pdict)
-                    redis_cli.lrem("zdsblist", 1, pjson)
+                    # pdict = {"1": user, "2": pwd, "3": batchid, "4": batchyear, "5": batchmonth, "6": companyid,
+                    #          "7": customerid, "8": host, "9": port, "10": db, "11": fw1, "12": fw2, "13": hw1,
+                    #          "14": hw2, "15": fwms, "16": fwyj, "17": hwyj, "18": hwms, "19": djzs, "20": dfzs,
+                    #          "21": dczs, "22": djms, "23": dfms, "24": dcms}
+                    # pjson = json.dumps(pdict)
+                    # redis_cli.lrem("zdsblist", 1, pjson)
+                    a = redis_cli.lrange('zdsblist', 0, -1)
+                    for i in a:
+                        if batchid in i:
+                            redis_cli.lrem('zdsblist', 1, i)
                     # ss=redis_cli.lpop("list")
                     # print(redis_cli.lpop("list"))
                     # result=run_test_suit.delay(user=account, pwd=pwd, batchid=batchid, batchyear=batchyear, batchmonth=batchmonth,companyid=companyid, customerid=customerid,host=host,port=port,db=db)
                     return HttpResponse("job is cancelled successfully~")
+                else:
+                    return HttpResponse("wrong message")
+
+            if post_data['Type'][0] == "TAXINVOICE":
+                logger.info("取消代开发票")
+                if post_data['BatchID'] and post_data['BatchYear'] and post_data['BatchMonth'] and post_data[
+                    'CompanyID'] and post_data['CustomerID'] and post_data['TaxId'] and post_data['TaxPwd'] and \
+                        post_data['jobname'] and post_data['jobparams']:
+                    logger.info("任务信息接收成功")
+                    user = post_data['TaxId'][0]
+                    pwd = post_data['TaxPwd'][0]
+                    batchid = post_data['BatchID'][0]
+                    batchyear = int(post_data['BatchYear'][0])
+                    batchmonth = int(post_data['BatchMonth'][0])
+                    companyid = int(post_data['CompanyID'][0])
+                    customerid = int(post_data['CustomerID'][0])
+                    jobname = post_data['jobname'][0]
+                    jobparams = post_data['jobparams'][0]
+                    # 获取数据库
+                    host, port, db = get_db(companyid)
+                    # 添加任务
+                    logger.info("取消任务到数据库")
+                    logger.info(db)
+                    cancelling_task(host, port, db, batchid, batchyear, batchmonth, companyid, customerid,
+                                    "TAXINVOICE", jobname, jobparams)
+                    logger.info("开始取消任务")
+                    a = redis_cli.lrange('daikai', 0, -1)
+                    for i in a:
+                        if batchid in i:
+                            redis_cli.lrem('daikai', 1, i)
+                    # pdict = {"1": user, "2": pwd, "3": batchid,  "4": companyid, "5": customerid, "6": host, "7": port, "8": db}
+                    # pjson = json.dumps(pdict)
+                    # redis_cli.lpush("daikai", pjson)
+                    # ss=redis_cli.lpop("list")
+                    # print(redis_cli.lpop("list"))
+                    # result=run_test_suit.delay(user=account, pwd=pwd, batchid=batchid, batchyear=batchyear, batchmonth=batchmonth,companyid=companyid, customerid=customerid,host=host,port=port,db=db)
+                    return HttpResponse("job is runing background~")
                 else:
                     return HttpResponse("wrong message")
 
